94eaea833e9d668fbdeb51caa2dffe29007af85e
totutote <tty102fnio@gmail.com>
2012-06-15 15:50:38 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnchantjsDemo::Application.config.secret_token = '236222f25f06471625174e66f6a7ae2d1b89c131638a5194aa51e96e0e62643aa21b331518c04844f87dcae331a3f4645c14d7d05fdfea120e1777abf3585f4b'
